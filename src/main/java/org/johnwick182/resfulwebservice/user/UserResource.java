875d9fe76a86fb4d0a3308744eff7499350802f6
Gabriel Guimaraes <dedito182@gmail.com>
2022-02-22 21:50:37 -0300

    implementing post

--- src/main/java/org/johnwick182/resfulwebservice/user/UserResource.java
+++ src/main/java/org/johnwick182/resfulwebservice/user/UserResource.java
@@ -1,9 +1,7 @@
 package org.johnwick182.resfulwebservice.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +19,11 @@ public class UserResource {
     @GetMapping("/users/{id} ")
     public User retrieveUser(@PathVariable int id) {
         return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        service.save(user);
     }
 
 
