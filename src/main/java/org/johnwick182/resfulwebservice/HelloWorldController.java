f8fb64bdecd08436bfa4ebb9d543d1c2fc58c3cd
Gabriel Guimaraes <dedito182@gmail.com>
2022-02-23 17:16:28 -0300

    languages with locale resolver spring

--- src/main/java/org/johnwick182/resfulwebservice/HelloWorldController.java
+++ src/main/java/org/johnwick182/resfulwebservice/HelloWorldController.java
@@ -1,10 +1,17 @@
 package org.johnwick182.resfulwebservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path="/hello-world")
     public String helloWorld() {
@@ -20,6 +27,13 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World %s", name));
     }
+
+    @GetMapping(path="/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required = false ) Locale locale) {
+        return messageSource.getMessage("good.morning.message", null, locale);
+    }
+
+
 
 
 }
